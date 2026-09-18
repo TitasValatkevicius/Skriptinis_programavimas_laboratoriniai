daf8b87d0bb3da07c64144956b4bf797043e4327
TitasValatkevicius <titas.valitas@gmail.com>
2026-09-18 11:34:28 +0300

    pirmas psl baigtas

--- LD1/LD2_TitasValatkevicius.m
+++ LD1/LD2_TitasValatkevicius.m
@@ -18,4 +18,25 @@ Z(:,2) = []
 Z = Z'
 
 % 3 uzduotis
+% duomenys
+A = 5
+f = 5
+q = 1,5
+U1 = 3
+U2 = 2
+t = 0:0.001:1
+s = A*sin(2*pi*f*t)
+n = q*randn(size(t))
+s = s+n
 % a)
+S1 = s(s > U1)
+% b)
+S2 = s
+S2(abs(s) < U2) = 0
+% c)
+S3 = length(s)
+% d)
+S4 = length(S1)
+% e)
+Maziausia = min(S2)
+Didziausia = max(S2)
